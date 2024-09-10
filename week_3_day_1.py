5a1a48ca03b27bdc2d4165d13bbdf7a358153826
Hernandez <ephernandez4@cps.k12.il.us>
2024-09-10 15:04:57 -0500

    whatever changes I made

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -50,16 +50,30 @@
 # # Review
 # create variables for the following :
 # 1. age
+age = 17 # integer variable
 # 2. name
+name = "Itzel" # string variable
 # 3. song
+song = "Promise" #string variable
 # 4. food
+food = "Tacos" # string variable
 # 5. number
-
+number = 7 # integer variable
 
 # #now include the variables you just made print in the following...
 
-
 # Once upon a time, there was a [age] old coder named [name].
-
+print("Once upon a time, there was a " + str(age) + " year old coder named " + name + ".") 
 
 # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+print("Itzel liked to hum the song " + song + " while coding. It was so annoying that their teamates would throw " + food + " until " + name + " would stop singing.")
+
+number1 = 1
+number2 = 2
+number3 = 3
+number4 = 4
+number5 = 5
+
+# print("Itzel has " + str(number1) + " dog. She also has " + str(number2) + " birds, " + str(number3) + " cats, " + str(number4) + " fish, and " + str(number5) + " hamsters.")
+#f strings
+print(f"{number1}")
